edc37b3a6378159fe579f518110fc8e31f117dfc
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-03-08 08:06:39 -0300

    ATIVIDADE

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $pessoa1 = array();
+    $pessoa2 = array();
+    $pessoa3 = array();
+    $pessoa4 = array();
+    
+
+    $pessoa1 [0][0][0][0][0][0] = "Joao";
+    $pessoa1 [0][0][0][0][1][1] = 16;
+    $pessoa1 [0][0][0][0][2][2] = "Sonserina";
+
+    $pessoa2 [0][0][0][0][0][0] = "Juan";
+    $pessoa2 [0][0][0][0][1][1] = 48;
+    $pessoa2 [0][0][0][0][2][2] = "Lufa-Lufa";
+
+    $pessoa3 [0][0][0][0][0][0] = "Joaquim";
+    $pessoa3 [0][0][0][0][1][1] = 35;
+    $pessoa3 [0][0][0][0][2][2] = "Grifinória";
+
+    $pessoa4 [0][0][0][0][0][0] = "Malu";
+    $pessoa4 [0][0][0][0][1][1] = 17;
+    $pessoa4 [0][0][0][0][2][2] = "Sonserina";
+
+    echo " O " . $pessoa1[0][0][0][0][0][0]  . " tem " . $pessoa1[0][0][0][0][1][1] . " e é da casa da " . $pessoa1[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa2[0][0][0][0][0][0] . " tem " . $pessoa2[0][0][0][0][1][1] . " e é da casa da " . $pessoa2[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa3[0][0][0][0][0][0] . " tem " . $pessoa3[0][0][0][0][1][1] . " e é da casa da " . $pessoa3[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa4[0][0][0][0][0][0] . " tem " . $pessoa4[0][0][0][0][1][1] . " e é da casa da " . $pessoa4[0][0][0][0][2][2];
+    ?>
+</body>
+</html>
